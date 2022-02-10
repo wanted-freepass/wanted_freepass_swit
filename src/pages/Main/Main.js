97a16ca309af0cb83f3fa6ff16c49ea9f7282026
yangddu <wndun21@naver.com>
2022-02-11 00:12:57 +0900

    Add: 줄바꿈 처리 구현중

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,10 +1,12 @@
 import React, { useState } from 'react';
 import * as S from './Main.style';
 import Modal from '../../components/Modal';
+import newLine from '../../utils/newLine';
 
 const Main = () => {
   const [text, setText] = useState('');
   const [commentList, setCommentList] = useState([]);
+
   const onChange = e => {
     setText(e.target.value);
   };
@@ -15,18 +17,27 @@ const Main = () => {
   };
 
   const onKeyPress = e => {
-    if (e.key == 'Enter') {
-      onClick();
+    if (e.keyCode == 13) {
+      if (e.shiftKey) {
+        newLine(text);
+      } else {
+        onClick();
+      }
     }
   };
+
   return (
     <S.Container>
       <form>
-        <textarea value={text} onChange={onChange} onKeyPress={onKeyPress} />
-
-        <button type="button" onClick={onClick}>
-          전송
-        </button>
+        <S.TextArea value={text} onChange={onChange} onKeyPress={onKeyPress} />
+        <S.TextDiv>{text}</S.TextDiv>
+        {text.length ? (
+          <button type="button" onClick={onClick}>
+            전송
+          </button>
+        ) : (
+          <button disabled="disabled">전송</button>
+        )}
       </form>
       <Modal />
     </S.Container>
--- src/pages/Main/Main.style.js
+++ src/pages/Main/Main.style.js
@@ -3,3 +3,9 @@ import styled from 'styled-components';
 export const Container = styled.div`
   width: 100%;
 `;
+
+export const TextArea = styled.textarea``;
+
+export const TextDiv = styled.div`
+  white-space: pre-line;
+`;
